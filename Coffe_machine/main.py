e40ab919a89db8e869d2a85e6d818c404f5cb81d
bright <obololastkiller@gmail.com>
2022-09-09 22:46:45 +0000

    Main updated

--- Coffe_machine/main.py
+++ Coffe_machine/main.py
@@ -88,12 +88,35 @@ profit = 0
 is_on = True
 
 """function: is_resource_sufficient"""
-def is_resource_sufficient(list_of_ingredients):
-    for item in list_of_ingredients:
-        if list_of_ingredients[item] >= resources[item]:
+def is_resource_sufficient(order_ingredients):
+    """Returns True when order can be made, False if ingredients are insufficient"""
+    for item in order_ingredients:
+        if order_ingredients[item] >= resources[item]:
             print(f"Sorry there is not enough {item}.")
             return False
     return True
+
+"""function: process_coins"""
+def process_coins():
+    """Returns the total calculated from coins inserted"""
+    print("Please insert coins.")
+    total = int(input("How many quarters?: ")) * 0.25
+    total += int(input("How many dimes?: ")) * 0.1
+    total += int(input("How many nickles?: ")) * 0.05
+    total += int(input("How many pennies?: ")) * 0.01
+
+
+def is_transaction_successful(money_received, drink_cost):
+    """Return True when the payment is accepted, or False if money is insufficient."""
+    if money_received >= drink_cost:
+        change = round(money_received - drink_cost, 2)
+        print(f"Here is ${change} in change.")
+        profit += drink_cost
+        return True
+    else:
+        print("Sorry that's not enough money. Money refunded.")
+        return False
+
 
 while is_on:
     choice = input("What would you like? (espresso/latte/cappuccino): ")
@@ -105,10 +128,12 @@ while is_on:
         print(f"Milk: {resources['milk']}ml")
         print(f"Coffee: {resources['coffee']}g")
         print(f"Money: ${profit}")
+        
     else:
         """we access a keyname using square bracket"""
         drink = MENU[choice]
-        is_resource_sufficient(drink["ingredients"])
+        if is_resource_sufficient(drink["ingredients"]):
+            payment = process_coins()
+            is_transaction_successful(payment, drink["cost"])
 
-# TODO:
 
